046730eb9aac3e79f6a627d7916d39597db3f45d
svivek19 <svivek030503@gmail.com>
2024-03-12 11:40:51 +0530

    task completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,65 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [numberOne, setNumberOne] = useState("");
+  const [numberTwo, setNumberTwo] = useState("");
+  const [total, setTotal] = useState(0);
+
+  const handleAdd = () => {
+    setTotal(parseFloat(numberOne) + parseFloat(numberTwo));
+  };
+
+  const handleSub = () => {
+    setTotal(parseFloat(numberOne) - parseFloat(numberTwo));
+  };
+
+  const handleMul = () => {
+    setTotal(parseFloat(numberOne) * parseFloat(numberTwo));
+  };
+
+  const handleDiv = () => {
+    setTotal(parseFloat(numberOne) / parseFloat(numberTwo).toFixed(2));
+  };
+
+  const handleClr = () => {
+    setNumberOne("");
+    setNumberTwo("");
+    setTotal(0);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <form onSubmit={(e) => e.preventDefault()}>
+        <label htmlFor="a">First Number</label>
+        <input
+          type="number"
+          id="a"
+          value={numberOne}
+          onChange={(e) => setNumberOne(e.target.value)}
+        />
+        <br />
+        <label htmlFor="b">Second Number</label>
+        <input
+          value={numberTwo}
+          type="number"
+          id="b"
+          onChange={(e) => setNumberTwo(e.target.value)}
+        />
 
-export default App
+        <div>
+          <br />
+          <button onClick={handleAdd}>ADD</button>
+          <button onClick={handleSub}>SUB</button>
+          <button onClick={handleMul}>MUL</button>
+          <button onClick={handleDiv}>DIV</button>
+          <button onClick={handleClr}>CLR</button>
+        </div>
+      </form>
+      <div>
+        <h2>Total: {total}</h2>
+      </div>
+    </div>
+  );
+};
+
+export default App;
